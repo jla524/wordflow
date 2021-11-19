f8c026efdf8b092247de3acccae21e0f33537752
Jacky Lee <jla524@sfu.ca>
2021-11-18 18:45:28 -0800

    Refactor scraper into separate functions

--- scraper.py
+++ scraper.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
-import praw
+"""
+A reddit scraper to get posts from subreddits
+Adapted from https://towardsdatascience.com/scraping-reddit-data-1c0af3040768
+"""
+from praw import Reddit
 from pandas import DataFrame
 from dotenv import dotenv_values
 
-# https://towardsdatascience.com/scraping-reddit-data-1c0af3040768
 
-# Load config and create a Reddit instance
-config = dotenv_values('.env')
-reddit = praw.Reddit(client_id=config['CLIENT_ID'],
-                     client_secret=config['CLIENT_SECRET'],
-                     user_agent=config['USER_AGENT'])
+def get_reddit() -> Reddit:
+    """Load config and create a Reddit instance"""
+    config = dotenv_values('.env')
+    instance = Reddit(client_id=config['CLIENT_ID'],
+                      client_secret=config['CLIENT_SECRET'],
+                      user_agent=config['USER_AGENT'])
+    return instance
 
-# Store top 10 posts in a list
-top10 = reddit.subreddit('dataengineering').hot(limit=10)
-posts = [[post.title, post.score, post.id, post.url,
-         post.selftext, post.created] for post in top10]
 
-# Convert posts to DataFrame
-columns = ['title', 'score', 'id', 'url', 'body', 'created']
-posts = DataFrame(posts, columns=columns)
+def get_posts(instance: Reddit, subreddit: str,
+              limit: int = 10) -> list[list[any]]:
+    """Store top 10 posts in a list"""
+    new_posts = instance.subreddit(subreddit).new(limit=limit)
+    results = [[post.title, post.score, post.id, post.url,
+                post.selftext, post.created] for post in new_posts]
+    return results
 
-print(posts)
+
+def to_frame(results: list[list[any]]) -> DataFrame:
+    """Convert results to DataFrame"""
+    columns = ['title', 'score', 'id', 'url', 'body', 'created']
+    results = DataFrame(results, columns=columns)
+    return results
+
+
+if __name__ == '__main__':
+    reddit = get_reddit()
+    posts = get_posts(reddit, 'dataengineering', limit=25)
+    posts = to_frame(posts)
+    print(posts)
